9b30720d5470201d4d1f7d99484811fd096602ef
Daniel <daniel@cloudspace.com>
2011-08-27 11:37:58 -0400

    WOOOO! it works!

--- test.rb
+++ test.rb
@@ -1,5 +1,9 @@
 require 'rjb'
 
+# A lot of help from the Using Weka in Java resources:
+# * http://weka.wikispaces.com/Use+WEKA+in+your+Java+code
+# *
+
 #Loader to use:
 #  weka.core.converters.CSVLoader
 #Filters to use:
@@ -15,7 +19,12 @@ require 'rjb'
 dir = "./weka.jar"
 
 #Have Rjb load the jar file, and pass Java command line arguments
-Rjb::load(dir, jvmargs=["-Xmx1000m"])
+Rjb::load(dir, jvmargs=["-Xmx1500m"])
+
+# loading some java classes
+Filter = Rjb::import("weka.filters.Filter")
+NullStemmer = Rjb::import("weka.core.stemmers.NullStemmer")
+WordTokenizer = Rjb::import("weka.core.tokenizers.WordTokenizer")
 
 #load the data using Java and Weka
 fanfics_src = Rjb::import("java.io.File").new("fanfics.csv")
@@ -23,10 +32,48 @@ fanfics_csvloader = Rjb::import("weka.core.converters.CSVLoader").new
 fanfics_csvloader.setFile(fanfics_src)
 fanfics_data = fanfics_csvloader.getDataSet
 
-# StringToWordVector
+# NominalToString
 nts = Rjb::import("weka.filters.unsupervised.attribute.NominalToString").new
-nts.setOptions ['-C last']
+nts.setOptions '-C last'.split(' ')
+nts.setInputFormat(fanfics_data)
+fanfics_data = Filter.useFilter(fanfics_data, nts)
 
+# Stemmer
+nullstemmer = Rjb::import("weka.core.stemmers.NullStemmer").new
+
+# Tokenizer
+wordtokenizer = Rjb::import("weka.core.tokenizers.WordTokenizer").new
+wordtokenizer.setOptions ["-delimiters", "\" \\r\\n\\t.,;:\\\'\\\"()?!\""]
+
+# StringToWordVector
+stwv = Rjb::import("weka.filters.unsupervised.attribute.StringToWordVector").new
+stwv.setOptions '-R first-last -W 1000 -prune-rate -1.0 -N 0 -S -M 1 -stopwords /Users/daniellewis/ruby-weka/stopwords.txt"'.split(' ')
+stwv.setStemmer nullstemmer
+stwv.setTokenizer wordtokenizer
+stwv.setInputFormat(fanfics_data)
+fanfics_data = Filter.useFilter(fanfics_data, stwv)
+
+# NumericToNominal
+ntn = Rjb::import("weka.filters.unsupervised.attribute.NumericToNominal").new
+ntn.setInputFormat(fanfics_data)
+fanfics_data = Filter.useFilter(fanfics_data, ntn)
+
+# Generate a classifier for each index
+(0..fanfics_data.numAttributes).each do |i|
+  # ADTree
+  adtree = Rjb::import("weka.classifiers.trees.ADTree").new
+  adtree.setOptions '-B 10 -E -3'.split(' ')
+  fanfics_data.setClassIndex(i)
+  adtree.buildClassifier fanfics_data
+  
+  # Write out to a dot file
+  classname = fanfics_data.classAttribute.toString.split(' ')[1]
+  graph = adtree.graph.gsub(/digraph ADTree {/, "digraph ADTree {\n#{classname}")
+  File.open('dots/'+classname+'.dot', 'w') { |f| f.write(graph) }
+  `dot -Tgif < dots/#{classname}.dot > gifs/#{classname}.gif`
+
+  puts "Generated tree for #{classname}"
+end
 
 
 #
